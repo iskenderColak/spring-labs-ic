350963291b89179fc94e0a70abed7aa3b4fc0bd2
iskenderColak <iclk84@gmail.com>
2022-11-19 22:29:48 +0300

    Login controller

--- lab-04-springmvc/src/main/java/com/icolak/controller/LoginController.java
+++ lab-04-springmvc/src/main/java/com/icolak/controller/LoginController.java
@@ -1,4 +1,33 @@
 package com.icolak.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class LoginController {
+
+    @RequestMapping("/login")
+    public String login(@RequestParam String email, @RequestParam String phoneNumber, Model model) {
+
+        model.addAttribute("email", email);
+        model.addAttribute("phoneNumber", phoneNumber);
+        model.addAttribute("loginMessage", "Login Successful");
+
+        return "login/login-info";
+    }
+/*
+    @RequestMapping("/login/{email}/{phoneNumber}")
+    public String login(@PathVariable String email, @PathVariable String phoneNumber, Model model) {
+
+        model.addAttribute("email", email);
+        model.addAttribute("phoneNumber", phoneNumber);
+        model.addAttribute("loginMessage", "Login Successful");
+
+        return "login/login-info";
+    }
+
+*/
 }
