dbf9f6a40c5594d306292b236d393793c7348ac2
iskenderColak <iclk84@gmail.com>
2022-11-19 14:37:42 +0300

    Lab controller

--- lab-04-springmvc/src/main/java/com/icolak/controller/LabController.java
+++ lab-04-springmvc/src/main/java/com/icolak/controller/LabController.java
@@ -1,5 +1,21 @@
 package com.icolak.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class LabController {
 
+    @RequestMapping("/lab")
+    public String showLab(Model model) {
+
+        model.addAttribute("firstLab", "lab-00-coupling");
+        model.addAttribute("secondLab", "lab-01-ioc");
+        model.addAttribute("thirdLab", "lab-02-di");
+        model.addAttribute("fourthLab", "lab-03-springboot");
+        model.addAttribute("fifthLab", "lab-04-springmvc");
+
+        return "lab/lab-list";
+    }
 }
